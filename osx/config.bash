61685a8ae2f8cf71ede00d0d40bc5de25d76924e
Andy Holman <andy@conspyre.com>
2010-11-16 22:07:20 -0500

    Add some items to config.bash

--- osx/config.bash
+++ osx/config.bash
@@ -19,3 +19,19 @@ echo -ne "Server time is: "; date
 
 export PATH="~/bin:~/.bin:~/.dotfiles/bin:/usr/local/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# notify of bg job completion immediately
+set -o notify
+
+# fuck that you have new mail shit
+unset MAILCHECK
+
+# disable core dumps
+ulimit -S -c 0
+
+# enable en_US locale w/ utf-8 encodings if not already configured
+: ${LANG:="en_US.UTF-8"}
+: ${LANGUAGE:="en"}
+: ${LC_CTYPE:="en_US.UTF-8"}
+: ${LC_ALL:="en_US.UTF-8"}
+export LANG LANGUAGE LC_CTYPE LC_ALL
